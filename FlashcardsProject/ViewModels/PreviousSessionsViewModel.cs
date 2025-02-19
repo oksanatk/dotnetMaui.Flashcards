10e15f99fded8304b375fd9f76ebed22ed7e4cb3
oksanatk <tkachoksn@gmail.com>
2025-02-19 16:01:13 -0500

    implemented INotifyPropertyChanged instead of ObservableObject

--- FlashcardsProject/ViewModels/PreviousSessionsViewModel.cs
+++ FlashcardsProject/ViewModels/PreviousSessionsViewModel.cs
@@ -1,16 +1,37 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input;
+﻿using CommunityToolkit.Mvvm.Input;
 using dotnetMAUI.Flashcards.Data;
 using dotnetMAUI.Flashcards.Models;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace dotnetMAUI.Flashcards.ViewModels;
 
-public partial class PreviousSessionsViewModel : ObservableObject
+public partial class PreviousSessionsViewModel : INotifyPropertyChanged
 {
     private readonly DbRepository _repository;
+    private int statsYear;
+    private string yearForStats = null!;
+    private bool chooseViewallSessions = true;
 
     public ObservableCollection<StudySession> AllStudySessions { get; set; } = new();
+    public string YearForStats { 
+        get => yearForStats; 
+        set { 
+            yearForStats = value; 
+            OnPropertyChanged(nameof(YearForStats)); 
+        } }
+    public bool ChooseViewAllSessions { 
+        get => chooseViewallSessions; 
+        set { 
+            chooseViewallSessions = value;
+            OnPropertyChanged(nameof(ChooseViewAllSessions));
+            OnPropertyChanged(nameof(ChooseViewStats));
+        } }
+    public bool ChooseViewStats => !ChooseViewAllSessions;
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged(string propertyName) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     
     public PreviousSessionsViewModel(DbRepository repository)
     {
@@ -29,6 +50,16 @@ public partial class PreviousSessionsViewModel : ObservableObject
         foreach (StudySession s in retreivedSessions)
         {
             AllStudySessions.Add(s);
+        }
+    }
+
+    [RelayCommand]
+    public void SubmitYearForStats()
+    {
+        if(int.TryParse(YearForStats, out statsYear))
+        {
+            //_repository.GetSessionsPerMonth(statsYear);
+            //_repository.GetAverageScoresPerMonths(statsYear);
         }
     }
 
